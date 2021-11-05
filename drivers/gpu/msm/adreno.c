8a5f96959c659e8e10a778c32abf8d047b4693e4
Sergey Vasiliev <sdv22@live.ru>
2021-11-05 10:33:32 +0300

    adreno: use stanadart qos

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -809,6 +809,8 @@ static int adreno_of_parse_pwrlevels(struct adreno_device *adreno_dev,
 		unsigned int index;
 		struct kgsl_pwrlevel *level;
 
+
+
 		if (adreno_of_read_property(child, "reg", &index))
 			return -EINVAL;
 
@@ -817,6 +819,7 @@ static int adreno_of_parse_pwrlevels(struct adreno_device *adreno_dev,
 				index);
 			continue;
 		}
+
 
 		if (index >= pwr->num_pwrlevels)
 			pwr->num_pwrlevels = index + 1;
@@ -1014,12 +1017,12 @@ static int adreno_of_get_power(struct adreno_device *adreno_dev,
 	/* get pm-qos-wakeup-latency, set it to default if not found */
 	if (of_property_read_u32(node, "qcom,pm-qos-wakeup-latency",
 		&device->pwrctrl.pm_qos_wakeup_latency))
-		device->pwrctrl.pm_qos_wakeup_latency = 100;
+		device->pwrctrl.pm_qos_wakeup_latency = 101;
 
 	/* override these */
-	device->pwrctrl.pm_qos_active_latency = 1000;
-	device->pwrctrl.pm_qos_cpu_mask_latency = 1000;
-	device->pwrctrl.pm_qos_wakeup_latency = 100;
+	//device->pwrctrl.pm_qos_active_latency = 1000;
+	//device->pwrctrl.pm_qos_cpu_mask_latency = 1000;
+	//device->pwrctrl.pm_qos_wakeup_latency = 100;
 
 	if (of_property_read_u32(node, "qcom,idle-timeout", &timeout))
 		timeout = 64;
