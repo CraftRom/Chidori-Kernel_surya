0db3d5aa7b3938a6b66e070e957f0b290ded8991
Sergey Vasiliev <sdv22@live.ru>
2021-11-22 06:47:50 +0300

    Merge remote-tracking branch 'chi/android-11' into HEAD

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
-# Compile script for Cartel kernel
+# Compile script for LOS kernel
+# Copyright (C) 2020-2021 Adithya R & @johnmart19.
 # Copyright (C) 2021 Craft Rom (melles1991).
 
 SECONDS=0 # builtin bash timer
@@ -20,24 +21,31 @@ echo -e "$blue░▐█▀█░▐█░▐█░▐██░▐█▀█▄▒
 echo -e "░▐█──░▐████─░█▌░▐█▌▐█▒▐█▄▒█▌▒▐█▒▐█─░█▌"
 echo -e "░▐█▄█░▐█░▐█░▐██░▐█▄█▀▒▐██▄█▌▒▐█▀▄▄░▐██$nocol"
 echo -e " "
-  
-# Main environtment
-KERNEL_DIR=$PWD
-KERN_IMG=$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb
-ZIP_DIR=$KERNEL_DIR/AnyKernel3
-CONFIG=vendor/surya-perf_defconfig
 
-# Export
-export ARCH=arm64
-export CROSS_COMPILE=$HOME/toolchains/gcc64/bin/aarch64-linux-androidkernel-
+ZIPNAME="Chidori-Kernel-surya-$(date '+%Y%m%d-%H%M').zip"
+TC_DIR="$HOME/toolchains/proton-clang"
+DEFCONFIG="vendor/surya-perf_defconfig"
+
+export PATH="$TC_DIR/bin:$PATH"
 export KBUILD_BUILD_USER=melles1991
 export KBUILD_BUILD_HOST=CraftRom-build
 
-
-echo -e "${txtbld}Config:${txtrst} $CONFIG"
-echo -e "${txtbld}ARCH:${txtrst} $ARCH"
+echo -e "${txtbld}Config:${txtrst} $DEFCONFIG"
+echo -e "${txtbld}ARCH:${txtrst} arm64"
 echo -e "${txtbld}Username:${txtrst} $KBUILD_BUILD_USER"
 echo -e " "
+
+if ! [ -d "$TC_DIR" ]; then
+echo "Proton clang not found! Cloning to $TC_DIR..."
+if ! git clone -q --depth=1 --single-branch https://github.com/kdrag0n/proton-clang $TC_DIR; then
+echo "Cloning failed! Aborting..."
+exit 1
+fi
+fi
+
+echo -e "$blue    \nMake DefConfig\n $nocol"
+mkdir -p out
+make O=out ARCH=arm64 $DEFCONFIG
 
 if [[ $1 == "-c" || $1 == "--clean" ]]; then
 if [  -d "./out/" ]; then
@@ -49,9 +57,8 @@ sleep 2
 fi
 
 if [[ $1 == "-r" || $1 == "--regen" ]]; then
-make $CONFIG
-cp .config arch/arm64/configs/$CONFIG
-git commit -am "defconfig: citrus: Regenerate" --signoff
+cp out/.config arch/arm64/configs/$DEFCONFIG
+git commit -am "defconfig: onclite: Regenerate" --signoff
 echo -e "$grn \nRegened defconfig succesfully!\n $nocol"
 make mrproper
 echo -e "$grn \nCleaning was successful succesfully!\n $nocol"
@@ -59,39 +66,27 @@ sleep 4
 exit 1
 fi
 
-# Main Staff
-clang_bin="$HOME/toolchains/proton-clang/bin"
-gcc_prefix64="aarch64-linux-gnu-"
-gcc_prefix32="arm-linux-gnueabi-"
-CROSS_COMPILE="aarch64-linux-gnu-"
-CROSS_COMPILE_ARM32="arm-linux-gnueabi-"
-
-_ksetup_old_path="$PATH"
-export PATH="$clang_bin:$PATH"
-
 # Build start
-echo -e "$blue    \nMake DefConfig\n $nocol"
-make	O=out $CONFIG
 echo -e "$blue    \nStarting kernel compilation...\n $nocol"
-make	-j`nproc --all` O=out ARCH=arm64 CC="clang" LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- Image.gz-dtb
+make -j$(nproc --all) O=out ARCH=arm64 CC="ccache clang" AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- Image.gz-dtb
 
-if ! [ -a $KERN_IMG ]; then
-    echo -e "$red \nKernel Compilation failed! Fix the errors!\n $nocol"
+
+if [ -f "out/arch/arm64/boot/Image.gz-dtb" ]; then
+echo -e "$blue    \nKernel compiled succesfully! Zipping up...\n $nocol"
+if ! [ -d "AnyKernel3" ]; then
+echo "AnyKernel3 not found! Cloning..."
+if ! git clone https://github.com/CraftRom/AnyKernel3 -b onclite AnyKernel3; then
+echo "Cloning failed! Aborting..."
 fi
-
-cd $ZIP_DIR
-make clean &>/dev/null
+fi
+cp out/arch/arm64/boot/Image.gz-dtb AnyKernel3
+rm -f *zip
+cd AnyKernel3
+zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder
 cd ..
-
-OUTDIR="$KERNEL_DIR/out/"
-cd libufdt/src && python2 mkdtboimg.py create $OUTDIR/arch/arm64/boot/dtbo.img $OUTDIR/arch/arm64/boot/dts/qcom/*.dtbo
-
-echo -e "$grn    \n(i)          Done moving modules\n $nocol"
-cd $ZIP_DIR
-cp $KERN_IMG zImage
-cp $OUTDIR/arch/arm64/boot/dtbo.img $ZIP_DIR
-make normal &>/dev/null
 echo -e "$grn \n(i)          Completed build$nocol $red$((SECONDS / 60))$nocol $grn minute(s) and$nocol $red$((SECONDS % 60))$nocol $grn second(s) !$nocol"
-echo -e "$blue    \n             Flashable zip generated under $yellow$ZIP_DIR.\n $nocol"
-cd ..
-# Build end
+echo -e "$blue    \n             Flashable zip generated $yellow$ZIPNAME.\n $nocol"
+rm -rf out/arch/arm64/boot
+else
+ echo -e "$red \nKernel Compilation failed! Fix the errors!\n $nocol"
+fi
