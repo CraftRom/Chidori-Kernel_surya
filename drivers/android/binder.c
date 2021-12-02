795ad1cda9bf192766f02639aeeb6e3ca10b5001
Forenche <prahul2003@gmail.com>
2021-12-03 00:16:54 +0530

    Merge tag 'v4.14.256' of https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux into rebase3
    This is the 4.14.256 stable release
    
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -2967,7 +2967,7 @@ static void binder_transaction(struct binder_proc *proc,
 		t->from = thread;
 	else
 		t->from = NULL;
-	t->sender_euid = task_euid(proc->tsk);
+	t->sender_euid = proc->cred->euid;
 	t->to_proc = target_proc;
 	t->to_thread = target_thread;
 	t->code = tr->code;
@@ -5330,7 +5330,6 @@ static int binder_open(struct inode *nodp, struct file *filp)
 	spin_lock_init(&proc->outer_lock);
 	get_task_struct(current->group_leader);
 	proc->tsk = current->group_leader;
-	mutex_init(&proc->files_lock);
 	proc->cred = get_cred(filp->f_cred);
 	INIT_LIST_HEAD(&proc->todo);
 	init_waitqueue_head(&proc->freeze_wait);
