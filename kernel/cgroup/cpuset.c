621597151231e95c4a7d98308c3877f16350b57c
Igor Kozlovskiy <igoryan94@gmail.com>
2022-02-11 18:47:34 +0300

    test: re-enable uclamp (maybe remove this later)

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -2131,11 +2131,11 @@ static void uclamp_set(struct kernfs_open_file *of,
 	const char *cs_name = cs->css.cgroup->kn->name;
 
 	static struct ucl_param tgts[] = {
-		{"top-app",             "35", "100", 1, 1},
-		{"foreground",          "0",  "75",  0, 0},
+		{"top-app",             "40", "100", 1, 1},
+		{"foreground",          "0",  "65",  0, 0},
 		{"restricted",          "0",  "50",  0, 0},
-		{"background",          "0",  "25",  0, 0},
-		{"system-background",   "0",  "50",  0, 0},
+		{"background",          "0",  "0",  0, 0},
+		{"system-background",   "0",  "30",  0, 0},
 		{"camera-daemon",       "0", "100",  0, 1},
 	};
 
