cb6a529e2148ebc288e4f71d537da33c07893062
Sergey Vasiliev <sdv22@live.ru>
2022-02-20 19:14:40 +0300

    Revert "BACKPORT: tcp: remove dead code in __tcp_v6_send_check()"
    This reverts commit 8ae176818399a29241f3ac52d9bb965fcc0d24c6.

--- include/net/ip6_checksum.h
+++ include/net/ip6_checksum.h
@@ -69,9 +69,15 @@ static inline void __tcp_v6_send_check(struct sk_buff *skb,
 {
 	struct tcphdr *th = tcp_hdr(skb);
 
-	th->check = ~tcp_v6_check(skb->len, saddr, daddr, 0);
-	skb->csum_start = skb_transport_header(skb) - skb->head;
-	skb->csum_offset = offsetof(struct tcphdr, check);
+	if (skb->ip_summed == CHECKSUM_PARTIAL) {
+		th->check = ~tcp_v6_check(skb->len, saddr, daddr, 0);
+		skb->csum_start = skb_transport_header(skb) - skb->head;
+		skb->csum_offset = offsetof(struct tcphdr, check);
+	} else {
+		th->check = tcp_v6_check(skb->len, saddr, daddr,
+					 csum_partial(th, th->doff << 2,
+						      skb->csum));
+	}
 }
 
 #if IS_ENABLED(CONFIG_IPV6)
