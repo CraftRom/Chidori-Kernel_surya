8db87f09314999f265f54da0450ce87fc8e50f19
Sergey Vasiliev <sdv22@live.ru>
2022-02-20 19:14:46 +0300

    Revert "BACKPORT: tcp: remove dead code after CHECKSUM_PARTIAL adoption"
    This reverts commit f65858c06c57dbb5a1b29076ee76cbb64b6b465c.

--- net/ipv4/tcp_ipv4.c
+++ net/ipv4/tcp_ipv4.c
@@ -560,9 +560,16 @@ void __tcp_v4_send_check(struct sk_buff *skb, __be32 saddr, __be32 daddr)
 {
 	struct tcphdr *th = tcp_hdr(skb);
 
-	th->check = ~tcp_v4_check(skb->len, saddr, daddr, 0);
-	skb->csum_start = skb_transport_header(skb) - skb->head;
-	skb->csum_offset = offsetof(struct tcphdr, check);
+	if (skb->ip_summed == CHECKSUM_PARTIAL) {
+		th->check = ~tcp_v4_check(skb->len, saddr, daddr, 0);
+		skb->csum_start = skb_transport_header(skb) - skb->head;
+		skb->csum_offset = offsetof(struct tcphdr, check);
+	} else {
+		th->check = tcp_v4_check(skb->len, saddr, daddr,
+					 csum_partial(th,
+						      th->doff << 2,
+						      skb->csum));
+	}
 }
 
 /* This routine computes an IPv4 TCP checksum. */
