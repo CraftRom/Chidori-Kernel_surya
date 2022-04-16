cc1a2595f1e4ff59e5c643ad7c88f8609e2be7cb
Sergey Vasiliev <sdv22@live.ru>
2022-04-16 08:19:35 +0300

    Don't panic on timeshift

--- kernel/sched/walt.c
+++ kernel/sched/walt.c
@@ -313,7 +313,7 @@ update_window_start(struct rq *rq, u64 wallclock, int event)
 	u64 old_window_start = rq->window_start;
 
 	delta = wallclock - rq->window_start;
-	BUG_ON(delta < 0);
+	//BUG_ON(delta < 0);
 	if (delta < sched_ravg_window)
 		return old_window_start;
 
